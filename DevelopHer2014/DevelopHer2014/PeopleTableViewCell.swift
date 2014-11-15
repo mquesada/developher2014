96e5423ac484a861764f381bf786ecd54a73fc9b
yakaolife <yakaolife@gmail.com>
2014-11-15 04:23:07 -0800

    People list tableView storyboard setup

---
+++ DevelopHer2014/DevelopHer2014/PeopleTableViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  PeopleTableViewCell.swift
+//  DevelopHer2014
+//
+//  Created by Ya Kao on 11/15/14.
+//  Copyright (c) 2014 Maricel-Betsy-Cindy-Alexa-Diana. All rights reserved.
+//
+
+import UIKit
+
+class PeopleTableViewCell: UITableViewCell {
+    //Name(firstname + last name)
+    //profileImage
+    //industry
+    //location
+    //headline
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var headLineLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var industryLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ DevelopHer2014/DevelopHer2014/controllers/PeopleListViewController.swift
@@ -0,0 +1,48 @@
+//
+//  PeopleListViewController.swift
+//  DevelopHer2014
+//
+//  Created by Ya Kao on 11/15/14.
+//  Copyright (c) 2014 Maricel-Betsy-Cindy-Alexa-Diana. All rights reserved.
+//
+
+import UIKit
+
+class PeopleListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 96
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //temp
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("PeopleCell") as PeopleTableViewCell
+        return cell
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if(segue.identifier == "ShowDetailSegue"){
+            println("Go to user detail")
+        }
+    }
+
+
+
+}
