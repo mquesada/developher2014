13584c32070bdeb9fd84581c1a19644ffeacf172
mquesada <mariqj@gmail.com>
2014-11-15 06:33:14 -0800

    Dummy invitation.

--- DevelopHer2014/DevelopHer2014/controllers/LoginViewController.swift
+++ DevelopHer2014/DevelopHer2014/controllers/LoginViewController.swift
@@ -28,6 +28,24 @@ class LoginViewController: UIViewController {
             println("User logged in")
         }
     }
+    @IBAction func sendInvitation(sender: AnyObject) {
+        var data = Dictionary<String, String>()
+        data["id"] = "1"
+        data["firstName"] = "Alexandra"
+        data["lastName"] = "Chakravarty"
+        data["headline"] = ""
+        data["pictureUrl"] = "https://media.licdn.com/mpr/mpr/shrink_200_200/p/2/005/00a/0d7/27fe2be.jpg"
+        data["emailAddress"] = "alexa.chakravarty@gmail.com"
+        data["industry"] = ""
+        
+        self.linkedInClient.sendInvitation(User(data: data), message: "Agregame por fa", success: { () -> Void in
+            println("ok")
+        }) { (error) -> Void in
+            println("failed")
+            println(error)
+        }
+        
+    }
     
     /*
     // MARK: - Navigation
