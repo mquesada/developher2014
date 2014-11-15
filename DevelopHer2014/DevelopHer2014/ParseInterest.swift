3e3e7b3ef9be5538bc2487c4c8b4ab2171764bd0
blky <jademails@hotmail.com>
2014-11-15 01:13:53 -0800

    adding files

--- DevelopHer2014/DevelopHer2014/ParseInterest.swift
+++ DevelopHer2014/DevelopHer2014/ParseInterest.swift
@@ -5,9 +5,6 @@
 //  Created by Cindy Zheng on 11/15/14.
 //  Copyright (c) 2014 Maricel-Betsy-Cindy-Alexa-Diana. All rights reserved.
 //
-
-
-
 import UIKit
 
 class ParseInterest: PFObject,PFSubclassing {
