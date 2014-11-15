255c17b039ef38f504e30e8a6e2457fd4c1c3d81
mquesada <mariqj@gmail.com>
2014-11-14 21:28:09 -0800

    Initial commit.

---
+++ DevelopHer2014/DevelopHer2014Tests/DevelopHer2014Tests.swift
@@ -0,0 +1,36 @@
+//
+//  DevelopHer2014Tests.swift
+//  DevelopHer2014Tests
+//
+//  Created by Maricel Quesada on 11/14/14.
+//  Copyright (c) 2014 Maricel-Betsy-Cindy-Alexa-Diana. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class DevelopHer2014Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
